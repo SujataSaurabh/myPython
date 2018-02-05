53ce1af6a9d577c6e9d73ef4bfa23fed8a99c600
Sujata Goswami <sugosw@HWS466aei.mpg.de>
2018-02-05 14:43:32 +0100

    using python as a calculator. This program exploits the use of mathematical operators in python

---
+++ calc.py
@@ -0,0 +1,13 @@
+#  This program explores the mathematical operators in python3
+#  source -- https://docs.python.org/3/tutorial/introduction.html#numbers
+#  SuGo
+
+print("-- PYTHON AS A CALCULATOR --")
+a = 2 + 2
+print ("sum of 2+2 = ", a)
+print("BODMAS rule test 1:  50-5*6 =   ", 50-5*6)
+print("BODMAS test 2: (50-5*6)/4 =   ", (50-5*6)/4)
+print("division (returns a floating point number): 8/5 =  ", 8/5)
+print("floor division:  17//3 = ", 17//3)
+print("% operator (returning the remainder)  17%3 = ", 17%3)
+print("** a power operator:  5**2 ", 5**2)
