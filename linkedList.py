911bd27a0f44c70730389950aa0dad9fb3bb0256
Su Go <sugo.1607@gmail.com>
2018-03-27 11:47:03 +0200

    added more functions to handle an unordered linked list. functions such as -- adding a node, search a node, print the linked list items, count the number of items

--- linkedList.py
+++ linkedList.py
@@ -18,14 +18,66 @@ class node:
          return self.next
 
     def setNext(self, newNext):
-        self.next =   newNext  # class and its function definition ends here
+        self.next = newNext  # class and its function definition ends here
 
+class unorderedList:
+    def __init__(self):
+        self.head = None
+
+    def isEmpty(self):
+        return self.head == None
+
+    def add(self, item):
+        temp = node(item)
+        temp.setNext(self.head)
+        self.head = temp
+        return True
+        ## linkedlist traversal functions --search, size and remove
+    def size(self):
+        current = self.head
+        count = 0
+        while current!= None:
+            count =  count+1
+            current = current.getNext()
+
+        return count
+
+    def printList(self):
+        current = self.head
+    #    print(current.getVal())
+        while current!= None:
+            print(current.getVal())
+            current = current.getNext()
+
+    def search(self, item):
+        current = self.head
+        itemFound = False
+        while current!=None and not itemFound:
+            if current.getVal() == item:
+                itemFound = True
+            else:
+                current  = current.getNext()
+        return itemFound
+
+# now use the class functions 
+myList  = unorderedList()
+print(myList.add(10))
+print(myList.add(11))
+print(myList.add(12))
+print(myList.add(22))
+
+size_ll = myList.size()
+print("Number of items in Linked list are: \t" + str(size_ll))
+print("The items in the linked list are: ")
+myList.printList()
+print(myList.search(22))
 # define an object of class node with name 'temp' and value '93'
-temp =  node(93)
+# temp =  node(93)
 # print its value
-print(temp.getVal())
+# print(temp.getVal())
 # set next node value
-temp.setNext(10)
-# print it 
-print(temp.getNext())
-print(temp.getVal())
+# temp.setNext(10)
+# temp.setNext(11)
+# print it
+# print(temp.getNext())
+# print(temp.getVal())
