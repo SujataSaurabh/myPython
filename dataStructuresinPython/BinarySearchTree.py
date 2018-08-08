345a10536effbbf62c949b21b182ed263128fad2
SujataSaurabh <35897531+SujataSaurabh@users.noreply.github.com>
2018-08-08 21:51:59 +0200

    function added to delete the smallest value node.
    Not validated yet!

--- dataStructuresinPython/BinarySearchTree.py
+++ dataStructuresinPython/BinarySearchTree.py
@@ -1,38 +1,6 @@
 #	@SuGo, 8 August 2018
 #	Implementation of Binary search tree
-'''
-class BStree:
-	def __init__(self):
-		self.key = self.value = None
-		self.left =self.right = None
-	def __setItem__(self, key, value):
-		if self.key == None:
-			self.key = key
-			self.value = value
-		elif key == self.key:
-			self.value = value
-		elif key<self.key:
-			if self.left:
-				self.left[key] = value
-			else:
-				self.left = BStree(key, value)
-		else:
-			if self.right:
-				self.right[key] = value
-			else:
-				self.right = BStree(key, value)
-	def __getItem__(self, key):
-		if self.key == None:
-			return None
-		elif key == self.key:
-			return self.value
-		elif key < self.key and self.left:
-			return self.left[key]
-		elif key > self.key and self.right:
-			return self.right[key]
-		else:
-			return None
-'''
+
 class Node:
 	def __init__(self, key):
 		self.left = self.right = None
@@ -53,21 +21,28 @@ def insert(root, node):
 			else:
 				insert(root.left, node)
 ## traversal (INORDER)
-def inorder(root):
+def inorder(root):   # Left Node Right
 	if root:
 		inorder(root.left)
 		print(root.value)
 		inorder(root.right)
-def preorder(root):
+def preorder(root):  # Node Left Right
 	if root:
 		print(root.value)
 		preorder(root.left)
 		preorder(root.right)
-def postorder(root):
+def postorder(root):  # Left Right Node
 	if root:
 		postorder(root.left)
 		postorder(root.right)
 		print(root.value)
+def minNode(root):
+	if root is None:
+		return root
+	else:	
+		if root.left is not None:
+			root = root.left
+	return root
 # 		**** 	test	****
 root = Node(50)
 insert(root, Node(20))
@@ -78,3 +53,4 @@ print("\n PREORDER")
 preorder(root)
 print("\n POSTORDER")
 postorder(root)
+minNode(root)
