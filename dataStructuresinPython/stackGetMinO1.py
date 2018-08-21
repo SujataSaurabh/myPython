74c8bae3336e42b158fcb4659b66ef941f266e4f
SujataSaurabh <35897531+SujataSaurabh@users.noreply.github.com>
2018-08-21 21:23:46 +0200

    the program stackGetMin. has still to be corrected

--- dataStructuresinPython/stackGetMinO1.py
+++ dataStructuresinPython/stackGetMinO1.py
@@ -2,7 +2,7 @@
 #	This program implements a stack. This stack is special because it returns the minimum 
 #	element of the stack in O(1) time and O(1) extra space.
 #	webref: https://www.geeksforgeeks.org/design-a-stack-that-supports-getmin-in-o1-time-and-o1-extra-space/
-
+# 	Still has to be corrected!!
 class specialStack():
 	def __init__(self):
 		self.items = []
